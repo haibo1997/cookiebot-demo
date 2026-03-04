f21a7ff09ff16fa0774299ad78fce9886dfb97f1
haibo.zhang <haibo.zhang@lotusflare.com>
2026-03-04 15:27:32 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,22 @@
     <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
     read the documentation
   </p>
+  <div
+    id="cookie-declaration-container"
+    style="border: 1px solid red; padding: 10px; margin: 10px"
+  >
+    <h2>Cookie Declaration</h2>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { onMounted } from "vue";
+onMounted(() => {
+  const el = document.createElement("script");
+  el.id = "CookieDeclaration";
+  el.src =
+    "https://consent.cookiebot.com/51fc488e-8bf3-462e-92f5-f0f46a8500fc/cd.js";
+  el.async = true;
+  document.getElementById("cookie-declaration-container").appendChild(el);
+});
+</script>
